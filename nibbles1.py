cd2ec8379aea721e1214100161316919bdb86943
Stephan Venter <gs.venter@gmail.com>
2021-02-19 10:55:54 +1300

    Added start and end screen and random apple

--- nibbles1.py
+++ nibbles1.py
@@ -1,14 +1,21 @@
 import pygame
+import random
+
 #initialize pygame
 pygame.init()
 pygame.font.init()
 
-#set up the screen and screen size
-screen = pygame.display.set_mode((700,700))
+
 
 #define variables
+screentype = 1 #1 = start screen, 2 = game screen, 3 =  end screen
+
+screen_width = 700
+screen_height = 700
+
 worm_x = [350,330,310,290]
 worm_y = [350,350,350,350]
+
 wormblocksize = 20;
 
 up = False;
@@ -16,12 +23,17 @@ down = False;
 left = False;
 right = False;
 
+
+#set up the screen and screen size
+screen = pygame.display.set_mode((screen_width, screen_height))
+
 #write some text
 def writetext(text, x, y, color=(0,0,0), fontsize=24):
     # you have to call this at the start, 
     myfont = pygame.font.SysFont('Arial', fontsize, False, False)
     textsurface = myfont.render(text, True, color)
     screen.blit(textsurface, (x, y))
+
 
 #draw worm
 def drawworm(wrm_x, wrm_y):
@@ -33,84 +45,124 @@ def drawworm(wrm_x, wrm_y):
         #draw worm body
         pygame.draw.circle(screen, (153, 0, 153), (worm_x[i], worm_y[i]), 15)
 
+#get random location
+def getrandomlocation(screenwidth, screenheight):
+    randx = random.randint(10, screenwidth - 10)
+    randy = random.randint(10, screenheight - 10)
 
+    return randx, randy
+
+
+#get first apple random location
+apple_x, apple_y = getrandomlocation(screen_width, screen_height)
 
 #initialize the main game loop
 running = True
 while running:
-    #fill background
-    screen.fill((92, 230, 131))
 
-    #speed of game
-    pygame.time.delay(70)
-
-    #go through events and see if close button was click
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                if(right):
-                    pass
-                else:
-                    left = True
-                    right, up, down = False, False, False
-            elif event.key == pygame.K_RIGHT:
-                if(left):
-                    pass
-                else:
-                    right = True
-                    left, up, down = False, False, False
-            elif event.key == pygame.K_UP:
-                if(down):
-                    pass
-                else:
-                    up = True
-                    right, left, down = False, False, False
-            elif event.key == pygame.K_DOWN:
-                if(up):
-                    pass
-                else:
-                    down = True
-                    right, left, up = False, False, False
     
- 
-    #draw a purple circle
-    #circle1 = pygame.draw.circle(screen, (204, 51, 255), (posx, posy), 20)
-    drawworm(worm_x, worm_y)
+    #start screen
+    if screentype == 1:
+        screen.fill((0, 0, 0))
+        writetext("Nibbles Reboot", 200, 200, (153, 0, 153), 48)
+        writetext("press enter to play", 250, 250, (255, 255, 255))
+        
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RETURN:
+                        screentype = 2
 
-    #move worm in direction
-    if(right):
-        worm_x.insert(0, worm_x[0] + wormblocksize)
-        worm_x.pop()
-        worm_y.insert(0, worm_y[0])
-        worm_y.pop()
+    #game screen
+    if screentype == 2:
+        #speed of game
+        pygame.time.delay(70)
 
-    if(left):
-        worm_x.insert(0, worm_x[0] - wormblocksize)
-        worm_x.pop()
-        worm_y.insert(0, worm_y[0])
-        worm_y.pop()
+        #fill background
+        screen.fill((92, 230, 131))
 
-    if(down):
-        worm_x.insert(0, worm_x[0] )
-        worm_x.pop()
-        worm_y.insert(0, worm_y[0] + wormblocksize)
-        worm_y.pop()
-
-    if(up):
-        worm_x.insert(0, worm_x[0])
-        worm_x.pop()
-        worm_y.insert(0, worm_y[0] - wormblocksize)
-        worm_y.pop()
-
-    #draw a red line
-    #pygame.draw.line(screen, (255, 153, 51), (30, 400), (670, 500), 5)
-    #add some text
-    #writetext('My Awesome Game', 0, 0)
-    #writetext('The is the second line', 0, 100, (255,0,0))
-    #writetext('And the third line', 0, 200, (0,0,255), 48)
+        #go through events and see if close button was click
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    if(right):
+                        pass
+                    else:
+                        left = True
+                        right, up, down = False, False, False
+                elif event.key == pygame.K_RIGHT:
+                    if(left):
+                        pass
+                    else:
+                        right = True
+                        left, up, down = False, False, False
+                elif event.key == pygame.K_UP:
+                    if(down):
+                        pass
+                    else:
+                        up = True
+                        right, left, down = False, False, False
+                elif event.key == pygame.K_DOWN:
+                    if(up):
+                        pass
+                    else:
+                        down = True
+                        right, left, up = False, False, False     
     
+        #draw a red circle
+        apple = pygame.draw.circle(screen, (255, 0, 0), (apple_x, apple_y), 15)
+        drawworm(worm_x, worm_y)
+
+        #move worm in direction
+        if(right):
+            worm_x.insert(0, worm_x[0] + wormblocksize)
+            worm_x.pop()
+            worm_y.insert(0, worm_y[0])
+            worm_y.pop()
+
+        if(left):
+            worm_x.insert(0, worm_x[0] - wormblocksize)
+            worm_x.pop()
+            worm_y.insert(0, worm_y[0])
+            worm_y.pop()
+
+        if(down):
+            worm_x.insert(0, worm_x[0] )
+            worm_x.pop()
+            worm_y.insert(0, worm_y[0] + wormblocksize)
+            worm_y.pop()
+
+        if(up):
+            worm_x.insert(0, worm_x[0])
+            worm_x.pop()
+            worm_y.insert(0, worm_y[0] - wormblocksize)
+            worm_y.pop()
+
+
+        #boundaries
+        if (worm_x[0] < 0) or (worm_x[0] > screen_width) or (worm_y[0] < 0) or (worm_y[0] > screen_height):
+            screentype = 3
+
+    #end screen
+    if screentype == 3:
+        screen.fill((0, 0, 0))
+        writetext("GAME OVER = YOU SUCK!!!", 100, 200, (255, 0, 0), 48)
+        writetext("press R to play again", 250, 250, (255, 255, 255))
+        
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_r:
+                        screentype = 2
+                        worm_x = [350,330,310,290]
+                        worm_y = [350,350,350,350]
+
+
     #update the game screen with latest
     pygame.display.update()
+
 pygame.quit()
